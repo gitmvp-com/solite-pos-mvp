3710b498e63115267e0d215fbb14335606602d38
Fili <126945152+filiksyos@users.noreply.github.com>
2025-10-22 14:48:57 +0300

    Initial commit: Solite POS MVP with product management and order taking

---
+++ app/src/main/java/com/socialite/solite_pos_mvp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.socialite.solite_pos_mvp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.socialite.solite_pos_mvp.ui.navigation.AppNavigation
+import com.socialite.solite_pos_mvp.ui.theme.SolitePOSTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SolitePOSTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    AppNavigation()
+                }
+            }
+        }
+    }
+}
